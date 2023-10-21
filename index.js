a28d0640773feb64c5841309afd566864e66af34
ilham-2001 <arizqyakbar@gmail.com>
2023-10-21 16:11:11 +0700

    tambah post

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 app.use(cors());
 
 app.get('/', (req, res)=> {
     res.send('HELLO WORLD!')
 });
 
-
 app.post('/auth', (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
 
+    res.send('AUTH IS COMPLETED');
 });
 
 app.listen(3000, () => {
